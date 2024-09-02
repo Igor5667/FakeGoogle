c7e41aed069108c0a33469f84f4a5457de81752a
Igor5567 <igidod@gmail.com>
2024-09-02 14:44:22 +0200

    add icons to DottedMenu

--- src/components/DottedMenu.tsx
+++ src/components/DottedMenu.tsx
@@ -5,30 +5,30 @@ import {
 } from "@material-tailwind/react";
 
 const moreApps = [
-  "Drive",
-  "Hangouts",
-  "Maps",
-  "Play",
-  "Photos",
-  "Play Music",
-  "Play Movies",
-  "Play Store",
-  "Google+",
-  "Grafika",
-  "Gmail",
-  "Grafika",
-  "Drive",
-  "Hangouts",
-  "Maps",
-  "Play",
-  "Photos",
-  "Play Music",
-  "Play Movies",
-  "Play Store",
-  "Google+",
-  "Grafika",
-  "Gmail",
-  "Grafika",
+  { name: "Drive", icon: "gmail-icon.png" },
+  { name: "Hangouts", icon: "hangouts-icon.png" },
+  { name: "Maps", icon: "gmail-icon.png" },
+  { name: "Play", icon: "hangouts-icon.png" },
+  { name: "Photos", icon: "gmail-icon.png" },
+  { name: "Play Music", icon: "hangouts-icon.png" },
+  { name: "Play Movies", icon: "gmail-icon.png" },
+  { name: "Play Store", icon: "hangouts-icon.png" },
+  { name: "Google+", icon: "gmail-icon.png" },
+  { name: "Grafika", icon: "hangouts-icon.png" },
+  { name: "Gmail", icon: "gmail-icon.png" },
+  { name: "Grafika", icon: "hangouts-icon.png" },
+  { name: "Drive", icon: "gmail-icon.png" },
+  { name: "Hangouts", icon: "hangouts-icon.png" },
+  { name: "Maps", icon: "gmail-icon.png" },
+  { name: "Play", icon: "hangouts-icon.png" },
+  { name: "Photos", icon: "gmail-icon.png" },
+  { name: "Play Music", icon: "hangouts-icon.png" },
+  { name: "Play Movies", icon: "gmail-icon.png" },
+  { name: "Play Store", icon: "hangouts-icon.png" },
+  { name: "Google+", icon: "gmail-icon.png" },
+  { name: "Grafika", icon: "hangouts-icon.png" },
+  { name: "Gmail", icon: "gmail-icon.png" },
+  { name: "Grafika", icon: "hangouts-icon.png" },
 ];
 
 function DottedMenu() {
@@ -44,10 +44,14 @@ function DottedMenu() {
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        {moreApps.map((app) => (
+        {moreApps.map(({ name, icon }) => (
           <div className="w-1/3 h-[100px] p-1 ">
-            <div className="h-full bg-google-gray-50 hover:bg-google-gray-100 p-2 rounded-full flex justify-center items-center text-center">
-              {app}
+            <div
+              className="h-full bg-google-gray-50 hover:bg-google-gray-100 p-2 rounded-full
+              justify-center items-center text-center flex flex-col"
+            >
+              <img src={icon} alt="gmail" className="w-[70%] " />
+              {name}
             </div>
           </div>
         ))}
