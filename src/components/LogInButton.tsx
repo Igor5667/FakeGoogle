8c4ebcb9a72a955a7e3a2fea2bd5f78a1e9752c3
Igor5567 <igidod@gmail.com>
2024-09-02 13:10:33 +0200

    add handleLogin

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -1,8 +1,15 @@
 import { BiLogIn } from "react-icons/bi";
 
 function LogInButton() {
+  const handleLogin = () => {
+    window.location.href = "https://accounts.google.com/";
+  };
+
   return (
-    <button className="bg-[#0b57d0] text-white  py-2 px-4 rounded-full">
+    <button
+      className="bg-[#0b57d0] text-white  py-2 px-4 rounded-full"
+      onClick={handleLogin}
+    >
       Zaloguj się
     </button>
   );
