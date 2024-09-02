d882cee25a42a3c67b16d797851f6a2ff51f003b
Igor5567 <igidod@gmail.com>
2024-09-02 14:29:32 +0200

    add and fixed DottedMenu

--- src/components/DottedMenu.tsx
+++ src/components/DottedMenu.tsx
@@ -4,6 +4,33 @@ import {
   PopoverContent,
 } from "@material-tailwind/react";
 
+const moreApps = [
+  "Drive",
+  "Hangouts",
+  "Maps",
+  "Play",
+  "Photos",
+  "Play Music",
+  "Play Movies",
+  "Play Store",
+  "Google+",
+  "Grafika",
+  "Gmail",
+  "Grafika",
+  "Drive",
+  "Hangouts",
+  "Maps",
+  "Play",
+  "Photos",
+  "Play Music",
+  "Play Movies",
+  "Play Store",
+  "Google+",
+  "Grafika",
+  "Gmail",
+  "Grafika",
+];
+
 function DottedMenu() {
   return (
     <Popover>
@@ -11,13 +38,19 @@ function DottedMenu() {
         <img src="dots-menu.png" alt="dots menu" />
       </PopoverHandler>
       <PopoverContent
-        className="w-52 h-52 p-0"
+        className="w-[300px] max-h-[300px] p-0 flex flex-wrap content-start overflow-y-auto text-black shadow-md"
         // that TypeScript wants
         placeholder={undefined}
         onPointerEnterCapture={undefined}
         onPointerLeaveCapture={undefined}
       >
-        hji adlkfjadl
+        {moreApps.map((app) => (
+          <div className="w-1/3 h-[100px] p-1 ">
+            <div className="h-full bg-google-gray-50 hover:bg-google-gray-100 p-2 rounded-full flex justify-center items-center text-center">
+              {app}
+            </div>
+          </div>
+        ))}
       </PopoverContent>
     </Popover>
   );
